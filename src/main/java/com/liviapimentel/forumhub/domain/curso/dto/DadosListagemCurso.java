498862400c91033e90d54ee212b7af4177b4ed20
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-26 12:47:15 -0300

    Add id nos DTOs

--- src/main/java/com/liviapimentel/forumhub/domain/curso/dto/DadosListagemCurso.java
+++ src/main/java/com/liviapimentel/forumhub/domain/curso/dto/DadosListagemCurso.java
@@ -2,9 +2,9 @@ package com.liviapimentel.forumhub.domain.curso.dto;
 
 import com.liviapimentel.forumhub.domain.curso.Curso;
 
-public record DadosListagemCurso(String nome, String categoria) {
+public record DadosListagemCurso(Long id, String nome, String categoria) {
 
     public DadosListagemCurso(Curso curso) {
-        this(curso.getNome(), curso.getCategoria());
+        this(curso.getId(), curso.getNome(), curso.getCategoria());
     }
 }
--- src/main/java/com/liviapimentel/forumhub/domain/usuario/dto/DadosListagemUsuario.java
+++ src/main/java/com/liviapimentel/forumhub/domain/usuario/dto/DadosListagemUsuario.java
@@ -4,9 +4,9 @@ import com.liviapimentel.forumhub.domain.usuario.Usuario;
 
 import java.util.stream.Collectors;
 
-public record DadosListagemUsuario(String nome, String email, String perfil) {
+public record DadosListagemUsuario(Long id, String nome, String email, String perfil) {
     public DadosListagemUsuario(Usuario usuario) {
-        this(usuario.getNome(),
+        this(usuario.getId(), usuario.getNome(),
                 usuario.getEmail(),
                 usuario.getPerfis().stream()
                         .map(Enum::name)

5c6970718d17cd7d6ce2323c4903a14418668828
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-27 20:53:35 -0300

    Recuperando o subject

--- src/main/java/com/liviapimentel/forumhub/infra/security/SecurityFilter.java
+++ src/main/java/com/liviapimentel/forumhub/infra/security/SecurityFilter.java
@@ -1,9 +1,13 @@
 package com.liviapimentel.forumhub.infra.security;
 
+import com.liviapimentel.forumhub.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,23 +16,32 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
+        var subject = tokenService.getSubject(tokenJWT);
 
-        System.out.println(tokenJWT);
-
+        // Chama o próximo filtro
         filterChain.doFilter(request, response);
 
     }
 
     private String recuperarToken(HttpServletRequest request) {
+
         var authorizationHeader = request.getHeader("Authorization");
+
         if (authorizationHeader == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
+            throw new RuntimeException("Token JWT não enviado ou inválido");
+
         }
 
-        return authorizationHeader;
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
--- src/main/java/com/liviapimentel/forumhub/infra/security/TokenService.java
+++ src/main/java/com/liviapimentel/forumhub/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.liviapimentel.forumhub.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.liviapimentel.forumhub.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,20 @@ public class TokenService {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
 
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Fórum Hub")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado");
+        }
     }
 
     private Instant dataExpiracao() {
